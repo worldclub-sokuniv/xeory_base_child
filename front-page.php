d7a238d5f492b710963068cf29327cbc9890aa79
mmfiber <masa.03191996@gmail.com>
2019-09-14 18:10:11 +0900

    add class

--- front-page.php
+++ front-page.php
@@ -20,8 +20,8 @@
 								foreach ( $posts as $post ) :
 									setup_postdata( $post );
 							?>
-								<div class="card-wrapper">
-									<a href="<?php the_permalink(); ?>" class="card front-page-card">
+								<div class="card-wrapper  front-page-card">
+									<a href="<?php the_permalink(); ?>" class="card">
 										<article class="card-content-wrapper">
 											<?php if ( get_the_post_thumbnail() ) { ?>
 												<div class="post-thumbnail" style="background-image: url(<?php the_post_thumbnail_url(); ?>)"></div>
