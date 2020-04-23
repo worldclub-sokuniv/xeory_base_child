bf0cc8ffa29b13e01677115a6aced3b447af7866
mmfiber <masa.03191996@gmail.com>
2020-04-24 01:14:56 +0900

    add interview ui

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
 const { src, dest, watch } = require("gulp")
 const sass = require("gulp-sass")
-const concat = require("gulp-concat")
+const rename = require("gulp-rename")
 
 const styles_root_path = "styles"
 
 const compileSass = () => {
-  src(styles_root_path+"/**/*.scss")
-  .pipe(
-    concat("style-sass.scss")
-  )
+  src(styles_root_path+"/main.scss")
   .pipe(
     sass({outputStyle: "expanded"})
   )
+  .pipe(rename("style-sass.css"))
   .pipe(dest("./"))
 }
 
