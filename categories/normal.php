a7f64dc25d2c5eacd31ca026e65a2f74b124afe7
mmfiber <42595867+mmfiber@users.noreply.github.com>
2020-05-19 19:33:21 +0900

    Feature issue23 filter by tags (#32)
    * add filter by tags
    
    * enable to detect all tags
    
    * add tag_filter
    
    * add endline
    
    * add endline

---
+++ categories/normal.php
@@ -0,0 +1,112 @@
+<?php 
+$qobj = get_queried_object();
+$catID = $qobj->cat_ID;
+
+$args = array(
+  'posts_per_page' => 5,
+  'orderby'        => 'date',
+  'order'          => 'DESC',
+  'category'       => $catID,
+);
+
+$tagIDs = [];
+if ( isset( $_POST ) ) {
+  foreach($_POST as $p){
+    $decoded = str_replace("\\", "", json_encode($p));
+    $length  = strlen($decoded);
+    $encoded = json_decode(substr($decoded, 1, $length-2));
+    $tagIDs[] = $encoded->term_id;
+  }
+  $args["tag__in"] = $tagIDs;
+}
+
+$tags = [];
+$includedIds = [];
+if (have_posts()){
+  while(have_posts()){
+    the_post();
+    $postTags = get_the_tags();
+    foreach($postTags as $tag){
+      if(!in_array($tag->term_id, $includedIds)){
+        array_push($tags, $tag);
+        array_push($includedIds, $tag->term_id);
+      }
+    }
+  }
+}
+
+$my_query = new wp_query( $args );
+?>
+
+<?php get_header(); ?>
+<div id="content">
+	<div class="wrap">
+		<?php bzb_breadcrumb(); ?>
+		<div id="main" <?php bzb_layout_main(); ?>>
+			<div class="main-inner">
+
+				<section class="cat-content">
+					<header class="cat-header">
+            <h1 class="post-title"><?php bzb_title(); ?></h1>
+            <?php tag_filter($tags, $tagIDs); ?>
+					</header>
+					<?php if ( is_category() ) { ?>
+						<?php bzb_category_description(); ?>
+					<?php } ?>
+				</section>
+
+				<div class="post-loop-wrap">
+        <?php
+				if ( $my_query->have_posts() ) :
+					while (  $my_query->have_posts() ) :
+            $my_query->the_post();
+        ?>
+
+						<article id="post-<?php echo the_ID(); ?>" <?php post_class(); ?> itemscope="itemscope" itemtype="http://schema.org/BlogPosting">
+							<header class="post-header">
+								<ul class="post-meta list-inline">
+									<li class="date updated" itemprop="datePublished" datetime="<?php the_time( 'c' ); ?>"><i class="fa fa-clock-o"></i> <?php the_time( 'Y.m.d' ); ?></li>
+								</ul>
+								<h2 class="post-title" itemprop="headline"><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h2>
+							</header>
+
+							<section class="post-content" itemprop="text">
+							<?php if ( get_the_post_thumbnail() ) { ?>
+								<div class="card-thumbnail" style="background-image: url(<?php the_post_thumbnail_url(); ?>)"></div>
+							<?php } ?>
+							<?php the_excerpt(); ?>
+							<a href="<?php the_permalink(); ?>" class="hover-btn read_more">続きを読む</a>
+							</section>
+						</article>
+							<?php
+
+					endwhile;
+
+					else :
+						?>
+
+				<article id="post-404"class="cotent-none post" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">
+					<section class="post-content" itemprop="text">
+						<?php echo get_template_part( 'content', 'none' ); ?>
+					</section>
+				</article>
+
+						<?php
+					endif;
+					?>
+				<?php
+				if ( function_exists( 'pagination' ) ) {
+					pagination( $wp_query->max_num_pages );
+				}
+				?>
+				</div><!-- /post-loop-wrap -->
+			</div><!-- /main-inner -->
+		</div><!-- /main -->
+
+	<?php get_sidebar(); ?>
+
+	</div><!-- /wrap -->
+
+</div><!-- /content -->
+
+<?php get_footer(); ?>
---
+++ libs/php/tag-filter.php
@@ -0,0 +1,31 @@
+<?php
+function tag_filter($tags, $selectedTags){
+  $chips = "";
+  foreach($tags as $tag){
+    if (in_array($tag->term_id, $selectedTags, true)) {
+      $tagName = $tag->name;
+      $tagId = $tag->term_id;
+      $encodedTag = json_encode($tag);
+      $chips .= <<< EOF
+      <div class="chip" value="{$tagId}">
+        <div class="chip-name">{$tagName}</div>
+        <div class="closebtn"></div>
+        <input name={$tagId} type="hidden" value={$encodedTag}>
+      </div>
+      EOF;
+    }
+  }
+
+  $encodedTags = json_encode($tags);
+  $form = <<< EOF
+    <form class="filter-by-tags" action="" method="post">
+      <input id="tag-search">
+      <button id="send" type="submit">検索</button>
+      <input id="available-tags" type="hidden" value={$encodedTags}>
+      <div id="filter-tags" class="chip-wrapper">{$chips}</div>
+    </form>
+  EOF;
+
+  echo $form;
+}
+?>
