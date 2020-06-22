c234dc66ffab31303757f48bf497f405c6132ad9
Seiichi Sato <satosei111997@gmail.com>
2020-06-22 12:14:07 +0900

    width, position, post-contents fix

--- front-page.php
+++ front-page.php
@@ -17,7 +17,7 @@
 							<div class="post-loop-wrap cards-section-wrapper">
 								<div class="section1-link">
 									<a href="http://worldclubsokalocal.local/category/article/" class="card">
-									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/happy_1567685308-768x512.jpg" width="585px" height="365px"> 
+									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/happy_1567685308-768x512.jpg"> 
 									<div class="image"></div>
 									<div class="titleset">
 										<h2 class="card-title">最新記事一覧</h2>
@@ -55,53 +55,46 @@
 				</div><!-- /section1 -->
 				<div class="section2">
 					<?php
-					$data = ["meta_key" => "views",
-										"posts_per_page" => 4, 
-										"orderby" => "meta_value_num",
+					$data = ["posts_per_page" => 4, 
+										"orderby" => "date",
 										"order" => "DESC",
-										"cat_title" => "人気記事"
+										"category_name" => "interview",
+										"cat_title" => "interview"
 									];
 						$cat_title = $data["cat_title"];
 						$posts = get_posts( $data );
 						if( $posts ): ?>
+						<div class="post-loop">
 							<div class="post-loop-wrap cards-section-wrapper">
 								<div class="section2-link">
 									<a href="http://worldclubsokalocal.local/category/article/" class="card">
-									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/happy_1567685324-1024x682.jpg" width="292.5px" height="235px"> 
+									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/happy_1567685324-1024x682.jpg" width="50%" height="235px"> 
 									<div class="image"></div>
 									<div class="titleset">
-										<h2 class="card-title">人気記事一覧</h2>
+										<h2 class="card-title">インタビュー記事一覧</h2>
 									</div><!-- /titleset -->
 									</a><!-- /card -->
 								</div><!-- /section2-link -->
-								<div class="hoge-link">
-									<a href="http://worldclubsokalocal.local/category/article/" class="card">
-									<div class="image"></div>
-									<div class="titleset">
-										<h2 class="card-title">??????</h2>
-									</div><!-- /titleset -->
-									</a><!-- /card -->
-								</div><!-- /hoge-link -->
 								<?php
 								foreach ( $posts as $post ) :
 									setup_postdata( $post );
 							?>
 								<div class="card-wrapper  front-page-card">
-								<a href="http://worldclubsokalocal.local/category/article/" class="label">POPS</a>
-									<a href="<?php the_permalink(); ?>" class="card">
-										<article class="card-content-wrapper">
-											<?php if ( get_the_post_thumbnail() ) { ?>
-													<div class="card-thumbnail" style="background-image: url(<?php the_post_thumbnail_url("full"); ?>)"></div>
-											<?php } ?>
-											<div class ="titleset">
-												<h2 class="card-title"><?php the_title(); ?></h2>
-												<span>
-													<i class="fa fa-clock-o"></i><?php the_time( 'Y.m.d' ); ?>
-												</span>
-											</div><!-- titleset -->
-										</article>
-									</a>
-									<h2 class="tags"><?php the_tags( '#', ' #' ); ?></h2>
+								<a href="http://worldclubsokalocal.local/category/article/" class="label">インタビュー</a>
+								<a href="<?php the_permalink(); ?>" class="card">
+									<article class="card-content-wrapper">
+										<?php if ( get_the_post_thumbnail() ) { ?>
+												<div class="card-thumbnail" style="background-image: url(<?php the_post_thumbnail_url("full"); ?>)"></div>
+										<?php } ?>
+										<div class ="titleset">
+											<h2 class="card-title"><?php the_title(); ?></h2>
+											<span>
+												<i class="fa fa-clock-o"></i><?php the_time( 'Y.m.d' ); ?>
+											</span>
+										</div><!-- titleset -->
+									</article>
+								</a>
+								<h2 class="tags"><?php the_tags( '#', ' #' ); ?></h2>
 								</div><!-- card-warapper -->
 							<?php endforeach; ?>
 							<?php
@@ -112,25 +105,35 @@
 								pagination($wp_query->max_num_pages);
 							} ?>
 							</div><!-- /post-loop-wrap -->
+						</div><!-- /post-loop -->
+						<div class="hoge-link">
+							<a href="http://worldclubsokalocal.local/category/article/" class="card">
+							<div class="image"></div>
+							<div class="titleset">
+								<h2 class="card-title">??????</h2>
+							</div><!-- /titleset -->
+							</a><!-- /card -->
+						</div><!-- /hoge-link -->
 				</div><!-- /section2 -->
-				<div class="section3">
+				<div class="section3"> 
 					<?php
 					$data = ["posts_per_page" => 3, 
 										"orderby" => "date",
 										"order" => "DESC",
-										"category_name" => "experience",
-										"cat_title" => "留学体験記"
+										"category_name" => "how to",
+										"cat_title" => "How to"
 									];
 						$cat_title = $data["cat_title"]; 	
 						$posts = get_posts( $data );
 						if( $posts ): ?>
+						<div class="post-loop">
 							<div class="post-loop-wrap cards-section-wrapper">
 								<?php
 								foreach ( $posts as $post ) :
 									setup_postdata( $post );
 									?>
 								<div class="card-wrapper  front-page-card">
-									<a href="http://worldclubsokalocal.local/category/experience/" class="label">留学体験記</a>
+									<a href="http://worldclubsokalocal.local/category/experience/" class="label">How to</a>
 									<a href="<?php the_permalink(); ?>" class="card">
 										<article class="card-content-wrapper">
 											<?php if ( get_the_post_thumbnail() ) { ?>
@@ -152,35 +155,46 @@
 							<?php if (function_exists("pagination")) {
 								pagination($wp_query->max_num_pages);
 							} ?>
-								<div class="section3-link">
-									<a href="http://worldclubsokalocal.local/category/experience/" class="card">
-									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/soccer_1567685338-1024x587.jpg" width="585px" height="365px"> 
-									<div class="image"></div>
-									<div class="titleset">
-										<h2 class="card-title">留学体験記一覧</h2>
-									</div><!-- /titleset -->
-									</a><!-- /card -->
-								</div><!-- /section3-link -->
 							</div><!-- /post-loop-wrap -->
+						</div><!-- /post-loop -->
+						<div class="section3-link">
+							<a href="http://worldclubsokalocal.local/category/experience/" class="card">
+							<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/soccer_1567685338-1024x587.jpg">
+							<div class="image"></div>
+							<div class="titleset">
+								<h2 class="card-title">How to記事一覧</h2>
+							</div><!-- /titleset -->
+							</a><!-- /card -->
+						</div><!-- /section3-link -->
 				</div><!-- /section3 -->
 				<div class="section4">
 					<?php
-					$data = ["posts_per_page" => 4,
-										"orderby" => "date",
+					$data = ["meta_key" => "views",
+										"posts_per_page" => 4, 
+										"orderby" => "meta_value_num",
 										"order" => "DESC",
-										"category_name" => "useful-info",
-										"cat_title" => "お役立ち情報"
+										"cat_title" => "人気記事"
 									];
 						$cat_title = $data["cat_title"];
 						$posts = get_posts( $data );
 						if( $posts ): ?>
+						<div class="post-loop">
 							<div class="post-loop-wrap cards-section-wrapper">
+								<div class="section4-link">
+									<a href="http://worldclubsokalocal.local/category/useful-info/" class="card">
+									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/question_1567857425-768x512.jpg" width="50%" height="235px"> 
+									<div class="image"></div>
+									<div class="titleset">
+										<h2 class="card-title">人気記事一覧</h2>
+									</div><!-- /titleset -->
+									</a><!-- /card -->
+								</div><!-- /section4-link -->
 								<?php
 								foreach ( $posts as $post ) :
 									setup_postdata( $post );
 									?>
 								<div class="card-wrapper  front-page-card">
-									<a href="http://worldclubsokalocal.local/category/useful-info/" class="label">お役立ち情報</a>
+									<a href="http://worldclubsokalocal.local/category/useful-info/" class="label">POPS</a>
 									<a href="<?php the_permalink(); ?>" class="card">
 										<article class="card-content-wrapper">
 											<?php if ( get_the_post_thumbnail() ) { ?>
@@ -202,24 +216,16 @@
 							<?php if (function_exists("pagination")) {
 								pagination($wp_query->max_num_pages);
 							} ?>
-								<div class="section4-link">
-									<a href="http://worldclubsokalocal.local/category/useful-info/" class="card">
-									<img src="https://worldclub-soka.com/wp-content/uploads/2019/09/question_1567857425-768x512.jpg" width="292.5px" height="235px"> 
-									<div class="image"></div>
-									<div class="titleset">
-										<h2 class="card-title">お役立ち情報一覧</h2>
-									</div><!-- /titleset -->
-									</a><!-- /card -->
-								</div><!-- /section4-link -->
-								<div class="hoge-link">
-									<a href="http://worldclubsokalocal.local/category/article/" class="card">
-									<div class="image"></div>
-									<div class="titleset">
-									<h2 class="card-title">??????</h2>
-									</div><!-- /titleset -->
-									</a><!-- /card -->
-								</div><!-- /hoge-link -->
 							</div><!-- /post-loop-wrap -->
+						</div><!-- /post-loop -->
+						<div class="hoge-link">
+							<a href="http://worldclubsokalocal.local/category/article/" class="card">
+							<div class="image"></div>
+							<div class="titleset">
+							<h2 class="card-title">??????</h2>
+							</div><!-- /titleset -->
+							</a><!-- /card -->
+						</div><!-- /hoge-link -->
 				</div><!-- /section4 -->
 			</div><!-- /sections -->
 		</div><!-- /main -->
