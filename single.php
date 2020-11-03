f61182037e3f70cf175eaaedb8a6aac33d1cdddb
Seiichi Sato <satosei111997@gmail.com>
2020-11-03 17:14:10 +0900

    add column

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php
-if ( in_category( array( 'interview', 'how-to' )) ) {
+if ( in_category( array( 'interview', 'how-to', 'column' )) ) {
   get_template_part( 'pages/interview' , false );
 } else {
   get_template_part( 'pages/normal' , false );
